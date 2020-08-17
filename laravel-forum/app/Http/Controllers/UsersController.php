9da3829c07e06eb0233437cca072587c461bc0ed
devmxv <martin@devmxv.me>
2020-08-17 12:17:38 -0500

    Display Notifications function

--- laravel-forum/app/Http/Controllers/UsersController.php
+++ laravel-forum/app/Http/Controllers/UsersController.php
@@ -9,6 +9,13 @@ class UsersController extends Controller
     //
     public function notifications(){
         //---Mark all as read
+        auth()->user()->unreadNotifications->markAsRead();
         //---after it displays all notifications
+
+        return view('users.notifications', [
+            'notifications' => auth()->user()->notifications()->paginate(5)
+
+
+        ]);
     }
 }
---
+++ laravel-forum/resources/views/users/notifications.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Notifications</div>
+
+                <div class="card-body">
+                    <ul class="list-group">
+                    @foreach($notifications as $notification)
+                        <li class="list-group-item">
+                            @if($notification->type === 'LaravelForum\Notifications\NewReplyAdded')
+                                A new reply was added to your discussion
+                                <strong>{{ $notification->data['discussion']['title'] }}</strong>
+                                <a href="{{ route('discussions.show', $notification->data['discussion']['slug']) }}" class="btn float-right btn-sm btn-info">
+                                    View discussion
+                                </a>
+                            @endif
+                        </li>
+                    @endforeach
+
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
